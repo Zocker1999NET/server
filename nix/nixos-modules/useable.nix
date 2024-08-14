4e6c62566bd44d6d2e527258778213bd1f2a0f37
Felix Stupp <felix.stupp@banananet.work>
2024-08-14 12:01:47 +0200

    nixos-mods/usuable: add my git default config

--- nix/nixos-modules/useable.nix
+++ nix/nixos-modules/useable.nix
@@ -94,9 +94,24 @@ in
 
       git = {
         enable = true;
-        # TODO config
-        init = {
-          defaultBranch = "git";
+        config = {
+          advice = {
+            detachedHead = true;
+          };
+          alias = {
+            lg1 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+            lg2 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
+            lg = ''!"git lg1"'';
+          };
+          core = {
+            autocrlf = "input";
+          };
+          init = {
+            defaultBranch = "main";
+          };
+          pull = {
+            ff = "only";
+          };
         };
       };
 
