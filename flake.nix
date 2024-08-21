113a92cebf3bc35ac9137e5bf1f0aaed7f97f4ef
Felix Stupp <felix.stupp@banananet.work>
2024-08-21 19:39:41 +0000

    flake: add input disko

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
     nixpkgs_unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # required submodules
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
