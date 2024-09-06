9e0bcf4fc8ce1313067beb4ab1261346997fa12b
Felix Stupp <felix.stupp@banananet.work>
2024-09-06 13:30:56 +0000

    nixosProfiles/common: add SSH host key of gitlab.kit.edu

--- nix/nixosProfiles/common.nix
+++ nix/nixosProfiles/common.nix
@@ -95,6 +95,7 @@ in
           "git.sr.ht".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZvRd4EtM7R+IHVMWmDkVU3VLQTSwQDSAvW0t2Tkj60";
           "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
           "gitlab.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
+          "gitlab.kit.edu".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOriuxsWoXA8CnvHndcG9c5u2lBXoCAmeFUhrMEMwY9+";
         };
       };
 
