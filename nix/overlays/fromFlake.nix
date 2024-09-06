5d6aded74efa75094eabfdc39842f2a205fca971
Felix Stupp <felix.stupp@banananet.work>
2024-09-06 13:32:35 +0000

    overlays/fromFlake: add librespot-auth

--- nix/overlays/fromFlake.nix
+++ nix/overlays/fromFlake.nix
@@ -3,6 +3,7 @@
 final: prev: {
   inherit (outputs.packages.${prev.system})
     # list all universally compatible packages from ./../packages
+    librespot-auth
     nft-update-addresses
     ;
 }
