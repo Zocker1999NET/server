6f64f1b6b1dc38c8f4ca55242deed6b3f3c2a13f
Felix Stupp <felix.stupp@banananet.work>
2025-01-06 18:58:12 +0000

    nixos-modules/router: rework workarounds implementation

--- nix/nixos-modules/router/interfaces/workarounds.nix
+++ nix/nixos-modules/router/interfaces/workarounds.nix
@@ -3,36 +3,74 @@ let
   inherit (globalArg) config lib;
   ifCfg = interface.config;
   wCfg = ifCfg.workarounds;
-  inherit (builtins) elem;
+
+  inherit (builtins)
+    concatStringsSep
+    elem
+    filter
+    mapAttrs
+    ;
+  inherit (lib.lists) singleton;
   inherit (lib.modules) mkIf mkMerge;
   inherit (lib.options) mkOption;
+  inherit (lib.trivial) flip pipe;
   systemdVersion = lib.toIntBase10 (lib.versions.major config.systemd.package.version);
-in
-{
 
-  options.workarounds = {
-    # options here supply additional information
-    # which are only required for determining if a workaround is required
-    # SHOULD BE without default & so required when used
-    dhcpv6PrefixDelegationWithoutAddress = mkOption {
+  # options here supply additional information
+  # which are only required for determining if a workaround is required
+  # SHOULD BE without default -> users are required to fill them in when relevant
+  workaroundInfos = {
+    dhcpv6PrefixDelegationWithoutAddress = {
       description = ''
         Describes whether the DHCPv6 server on this network (provided by the ISP)
         is only providing prefix delegations (IA_PD)
         while not providing non-temporary addresses (IA_NA).
-
-        As of now, this information is used for following workarounds:
-        - https://github.com/systemd/systemd/issues/31349#issuecomment-1949165385 (systemd <= 255)
       '';
       type = lib.types.bool;
     };
   };
 
-  config = mkMerge [
-    (mkIf (
-      systemdVersion <= 255
-      && elem "dhcp-pd" ifCfg.routing.ipv6DownstreamModes
-      && wCfg.dhcpv6PrefixDelegationWithoutAddress
-    ) { networkd.dhcpV6Config.UseAddress = false; })
+  # "requiredInfo" is only used to build description of info options
+  workarounds = [
+    {
+      description = ''
+        https://github.com/systemd/systemd/issues/31349#issuecomment-1949165385 (systemd <= 255)
+      '';
+      requiredInfo = singleton "dhcpv6PrefixDelegationWithoutAddress";
+      condition = (
+        systemdVersion <= 255
+        && elem "dhcp-pd" ifCfg.routing.ipv6DownstreamModes
+        && wCfg.dhcpv6PrefixDelegationWithoutAddress
+      );
+      action.networkd.dhcpV6Config.UseAddress = false;
+    }
+  ];
+
+in
+{
+
+  options.workarounds = flip mapAttrs workaroundInfos (
+    n: i:
+    mkOption (
+      i
+      // {
+        description = ''
+          ${i.description}
+
+          In general, these workaround infos are required to be known at build time
+          because the used software cannot dynamically detect if these workarounds are required or not.
+
+          As of now, this information is used for following workarounds:
+
+          - ${concatStringsSep "\n- " (filter (w: elem n w.requiredInfo) workarounds)}
+        '';
+      }
+    )
+  );
+
+  config = pipe workarounds [
+    (map (w: mkIf w.condition w.action))
+    mkMerge
   ];
 
 }
