2a43e76b8df095681b9215d9fc1eae21e10e493a
Felix Stupp <felix.stupp@banananet.work>
2025-04-06 17:16:48 +0000

    nixos-mod/router: extract dns stuff into its own module file & dir

--- nix/nixos-modules/router/default.nix
+++ nix/nixos-modules/router/default.nix
@@ -128,6 +128,8 @@ in
   imports = importsApplyMods [
     # modules
     outputs.nixosModules.systemd-radv-fadeout
+    # directories
+    ./dns
     # files
     ./references.nix
     # compatibility with third-party services
@@ -178,47 +180,6 @@ in
           example = [ "AA:BB:CC:DD:EE:FF" ];
         };
       */
-
-      dns = {
-        upstreams = lib.mkOption {
-          description = "List of DNS servers used by DNS server for clients.";
-          type = lib.types.listOf lib.types.str;
-        };
-        fallbacks = lib.mkOption {
-          description = "List of DNS servers used as fallbacks (i.e. when all upstreams are unreachable). Used by DNS server for clients.";
-          type = lib.types.listOf lib.types.str;
-          default = [ ];
-        };
-        bootstraps = lib.mkOption {
-          description = "List of DNS servers used to bootstrap list of other DNS servers. Used by DNS server for clients.";
-          type = lib.types.listOf lib.types.str;
-          default = cfg.dns.localFallbacks;
-          defaultText = lib.literalExpression "config.x-banananetwork.routerVM.dns.localFallbacks";
-        };
-        localFallbacks = lib.mkOption {
-          description = "List of DNS servers used as local fallbacks, i.e. added aside of own DNS server to {file}`/etc/resolv.conf` of router itself.";
-          type = with lib.types; listOf ipAddress;
-          default = [ ];
-        };
-        webui.username = lib.mkOption {
-          description = "Username for AdGuard Home admin account.";
-          type = lib.types.str;
-        };
-        webui.password = lib.mkOption {
-          description = ''
-            Hash for AdGuard Home admin account.
-            Can be created with .e.g. {command}`mkpasswd --method=bcrypt`'
-
-            For more info, read in the [Adguard Home Wiki](https://github.com/AdguardTeam/AdguardHome/wiki/Configuration#reset-web-password)
-          '';
-          type = lib.types.str;
-        };
-        filterlists = lib.mkOption {
-          description = "List of URLs of filterlists which entries should be blocked";
-          type = with lib.types; listOf str;
-          default = [ ];
-        };
-      };
 
       interfaces = lib.mkOption {
         description = ''
@@ -520,78 +481,6 @@ in
             (builtins.elemAt x 0).nft-update-addresses.config
           ))
         ];
-      };
-    };
-
-    # DNS server
-    # TODO (feature) migrate to blocky, seems way better & automated for our use case: https://0xerr0r.github.io/blocky/latest/
-    # TODO (feature) exclude router from filtering itself (without config)
-    # TODO (feature) add support for https://opennic.org/
-    # TODO (upstream, minor) change NixOS upstream module to remove yaml-merge dependency if not required
-
-    services.adguardhome = {
-      enable = true;
-      allowDHCP = false;
-      mutableSettings = false;
-      port = 3000;
-      settings = {
-        http = {
-          session_ttl = "5:00";
-        };
-        users = lib.singleton {
-          name = "admin";
-          password = ""; # TODO bcrypt htpasswd random stuff
-        };
-        auth_attempts = 5;
-        block_auth_min = 5;
-        dns = {
-          port = 53;
-          anonymize_client_ip = false;
-          ratelimit = 100; # queries / second
-          ratelimit_subnet_len_ipv4 = 32;
-          ratelimit_subnet_len_ipv6 = 128;
-          refuse_any = true;
-          upstream_dns = cfg.dns.upstreams;
-          fallback_dns = cfg.dns.fallbacks;
-          bootstrap_dns = cfg.dns.bootstraps;
-          local_ptr_upstreams = [
-            # TODO
-          ];
-          upstream_mode = "load_balance";
-          upstream_timeout = "10s";
-          use_http3_upstream = true;
-          enable_dnssec = true;
-          cache_time = 36000;
-          resolve_clients = true;
-          serve_plain_dns = true;
-          hostsfile_enabled = false;
-        };
-        filtering = {
-          protection_enabled = true;
-          filtering_enabled = true;
-          blocking_mode = "nxdomain";
-          blocked_response_ttl = 60; # seconds
-          safe_search.enabled = false;
-          safebrowsing_enabled = true;
-        };
-        querylog = {
-          enabled = true;
-          file_enabled = true;
-          interval = "${toString (2 * 24)}h";
-        };
-        statistics = {
-          enabled = true;
-          interval = "${toString (7 * 24)}h";
-        };
-        filters = lib.trivial.flip map cfg.dns.filterlists (url: {
-          enabled = true;
-          inherit url;
-          name = url;
-          ID = builtins.hashString "sha1" url;
-        });
-        dhcp.enabled = false;
-        tls.enabled = false;
-        log.file = "syslog"; # journal
       };
     };
 
