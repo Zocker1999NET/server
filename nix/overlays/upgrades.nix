d56947c56ff9c6b10c26da6dc2aed1ff803cd0ad
Felix Stupp <felix.stupp@banananet.work>
2025-04-22 06:37:40 +0000

    upgrade pferd to 3.8.1

--- nix/overlays/upgrades.nix
+++ nix/overlays/upgrades.nix
@@ -1,6 +1,15 @@
 { lib, ... }@flakeArg:
 { pkgs, ... }@systemArg:
 final: prev: {
+  pferd = prev.pferd.overrideAttrs (old: rec {
+    version = "3.8.1";
+    src = pkgs.fetchFromGitHub {
+      owner = "Garmelon";
+      repo = "PFERD";
+      tag = "v${version}";
+      sha256 = "sha256-IRQQkQTkP0B3S8j2MFP5W18wt6QsZ5MppAwvOUfE1Yg=";
+    };
+  });
   ptouch-print = prev.ptouch-print.overrideAttrs (old: {
     version = "1.6";
     src = pkgs.fetchgit {
