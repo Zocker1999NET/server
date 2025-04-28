b4d5b5b7e60b952d55794c983e666b1c133e711e
Felix Stupp <felix.stupp@banananet.work>
2025-04-28 09:44:32 +0000

    lib.modules: add todo to upstream importApplyIf & mkTestOverride

--- nix/lib/modules.nix
+++ nix/lib/modules.nix
@@ -6,11 +6,13 @@ let
 in
 {
 
+  # TODO upstream
   importsApplyIf = staticArg: map (i: if isPath i || isString i then importApply i staticArg else i);
 
   importApplyMod = path: importApply path flakeArg;
   importsApplyMods = importsApplyIf flakeArg;
 
+  # TODO upstream
   mkTestOverride = mkOverride 55;
 
 }
