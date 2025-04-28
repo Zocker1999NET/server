6d71162770426cd0011372844677ed24b12ad33c
Felix Stupp <felix.stupp@banananet.work>
2025-04-28 09:41:46 +0000

    lib.modules: add importApply helper functions

--- nix/lib/modules.nix
+++ nix/lib/modules.nix
@@ -1,9 +1,15 @@
 { inputs, lib, ... }@flakeArg:
 let
+  inherit (builtins) isPath isString;
   inherit (lib.backport) backportNixpkg;
-  inherit (lib.modules) mkOverride;
+  inherit (lib.modules) importApply importsApplyIf mkOverride;
 in
 {
+
+  importsApplyIf = staticArg: map (i: if isPath i || isString i then importApply i staticArg else i);
+
+  importApplyMod = path: importApply path flakeArg;
+  importsApplyMods = importsApplyIf flakeArg;
 
   mkTestOverride = mkOverride 55;
 
