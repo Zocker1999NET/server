51b33e14305d30c99f1b00c7aea29764b4cb9a74
Felix Stupp <felix.stupp@banananet.work>
2025-04-30 08:23:56 +0000

    add config for iehsrv995

--- nix/nixos/default.nix
+++ nix/nixos/default.nix
@@ -213,6 +213,76 @@ in
     system = "x86_64-linux";
   };
 
+  "iehsrv995" = nixosSystem {
+    # TODO copy config from host
+    modules = [
+      # TODO move into own modules
+      (
+        { config, ... }:
+        {
+          services.btrfs.autoScrub = {
+            enable = true;
+            interval = "weekly";
+          };
+          nix.sshServe = {
+            enable = true;
+            keys = config.x-banananetwork.sshPublicKeys;
+            protocol = "ssh";
+            write = true;
+          };
+          nix.settings.trusted-users = [ "nix-ssh" ];
+        }
+      )
+      # host config
+      {
+        networking.domain = "ieh.kit.edu";
+        networking.hostName = "iehsrv995";
+        x-banananetwork.useable.enable = true;
+        x-banananetwork.userName = "iehadmin";
+        x-banananetwork.vmCommon.enable = true;
+      }
+      # hardware
+      outputs.nixosProfiles.pveGuest
+      # installation state
+      (
+        let
+          btrfsRootOpts = [ "compress=zstd" ];
+        in
+        {
+          fileSystems."/" = {
+            device = "/dev/disk/by-uuid/cc109c2d-c70a-481d-9f1a-4c5c2e9cc964";
+            fsType = "btrfs";
+            options = [ "subvol=root" ] ++ btrfsRootOpts;
+          };
+          fileSystems."/home" = {
+            device = "/dev/disk/by-uuid/cc109c2d-c70a-481d-9f1a-4c5c2e9cc964";
+            fsType = "btrfs";
+            options = [ "subvol=home" ] ++ btrfsRootOpts;
+          };
+          fileSystems."/nix" = {
+            device = "/dev/disk/by-uuid/cc109c2d-c70a-481d-9f1a-4c5c2e9cc964";
+            fsType = "btrfs";
+            options = [ "subvol=nix" ] ++ btrfsRootOpts;
+          };
+          fileSystems."/boot" = {
+            device = "/dev/disk/by-uuid/C551-A77E";
+            fsType = "vfat";
+            options = [
+              "fmask=0077"
+              "dmask=0077"
+            ];
+          };
+          swapDevices = [ { device = "/dev/disk/by-uuid/42ac2839-0bb1-45ac-ad00-6b49142bf965"; } ];
+        }
+      )
+      {
+        system.stateVersion = "24.05";
+        x-banananetwork.sshHostPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJlrzgVqPcIUbkMHVOQZgee9j2CwfDiW4oJhUKdIcQKV iehsrv995";
+      }
+    ];
+    system = "x86_64-linux";
+  };
+
   /*
     nixosInstPlasma = nixpkgs.lib.nixosSystem {
       modules = lib.singleton (
