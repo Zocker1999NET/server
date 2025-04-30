8f8de319ae86fa91e326e4a6ed8404853274a8e1
Felix Stupp <felix.stupp@banananet.work>
2025-04-30 08:28:52 +0000

    add work in progress hmModules files

---
+++ nix/hmModules/frontend/base.nix
@@ -0,0 +1,25 @@
+# this stuff replaces all settings which would be configured by the corresponding frontend NixOS module
+
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.x-banananetwork.frontend;
+in
+{
+
+  config = lib.mkIf (cfg.enable && !cfg.nixosModuleCompat) {
+
+    assertions = [
+      {
+        assertion = !cfg.nixosModuleCompat;
+        message = "missing implementation of base stuff";
+      }
+    ];
+
+  };
+
+}
---
+++ nix/hmModules/frontend/extension.nix
@@ -0,0 +1,9 @@
+# this stuff must all be compatible to settings already configured by the corresponding frontend NixOS module
+
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{ }
