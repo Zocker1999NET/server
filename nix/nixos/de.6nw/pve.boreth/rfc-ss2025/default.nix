fccd0272f68d7dffc1eb1cc6876fa45ec085a486
Felix Stupp <felix.stupp@banananet.work>
2025-06-26 22:25:20 +0000

    rfc-ss2025: remove not required DB parts

--- nix/nixos/de.6nw/pve.boreth/rfc-ss2025/default.nix
+++ nix/nixos/de.6nw/pve.boreth/rfc-ss2025/default.nix
@@ -5,53 +5,6 @@ in
 {
   modules = [
 
-    # DB parts
-    (
-      { config, pkgs, ... }:
-      {
-        networking.firewall = {
-          allowedTCPPorts = [
-            3389
-          ];
-        };
-        services.mysql = {
-          enable = true;
-          ensureDatabases = singleton "ethnodes";
-          ensureUsers = [
-            {
-              name = "zocker";
-              ensurePermissions."*.*" = "ALL PRIVILEGES";
-            }
-          ];
-          package = pkgs.mariadb;
-        };
-        virtualisation.containers.enable = true;
-        virtualisation = {
-          oci-containers = {
-            backend = "podman";
-            containers = {
-              phpmyadmin = {
-                environment = {
-                  "PMA_HOST" = "host.containers.internal";
-                };
-                image = "docker.io/library/phpmyadmin:latest";
-                ports = [
-                  "80:80"
-                ];
-              };
-            };
-          };
-          podman = {
-            enable = true;
-            # Create a `docker` alias for podman, to use it as a drop-in replacement
-            dockerCompat = true;
-            # Required for containers under podman-compose to be able to talk to each other.
-            defaultNetwork.settings.dns_enabled = true;
-          };
-        };
-      }
-    )
-
     # config
     (
       { config, pkgs, ... }:
