0c914db1ea77b49746e86cb8b1ff02d81c9756d0
Felix Stupp <felix.stupp@banananet.work>
2025-07-18 22:17:27 +0000

    nixos-mod/frontend: enable ZSH history scrolling with prefix filter

--- nix/nixos-modules/frontend/home.nix
+++ nix/nixos-modules/frontend/home.nix
@@ -724,6 +724,15 @@ in
         # Disable flow control (^S/^Q freezing terminal)
         stty -ixon
 
+        # configure history scroll to search by prefix (https://superuser.com/a/585004)
+        autoload -U up-line-or-beginning-search
+        autoload -U down-line-or-beginning-search
+        zle -N up-line-or-beginning-search
+        zle -N down-line-or-beginning-search
+        # (https://unix.stackexchange.com/a/405358)
+        bindkey "''${terminfo[kcuu1]}" up-line-or-beginning-search # Up
+        bindkey "''${terminfo[kcud1]}" down-line-or-beginning-search # Down
+
         # autoopen files
         alias -s json="jq <"
 
