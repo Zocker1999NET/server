f9eb0ee59d57cfa2ebbb43192e69d92789bf7e9b
Felix Stupp <felix.stupp@banananet.work>
2025-09-19 11:28:31 +0000

    backport ncspot until 25.11 via overlay
    for getting the fix of https://github.com/NixOS/nixpkgs/pull/436861

--- nix/overlays/backports.nix
+++ nix/overlays/backports.nix
@@ -1,6 +1,7 @@
 { lib, ... }@flakeArg:
 { pkgs_unstable, ... }@systemArg:
 lib.backport.backportingOverlay pkgs_unstable {
+  ncspot = "25.11"; # or https://github.com/NixOS/nixpkgs/pull/438048
   # should always be compatible & improve experience
   retroarch-joypad-autoconfig = "99.99";
 }
