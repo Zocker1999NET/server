766df06432c420090de3eb4ac542491fe8d43f70
Felix Stupp <felix.stupp@banananet.work>
2025-09-19 11:28:04 +0000

    overlays/backports: remove taskwarrior3, was until 25.05

--- nix/overlays/backports.nix
+++ nix/overlays/backports.nix
@@ -1,7 +1,6 @@
 { lib, ... }@flakeArg:
 { pkgs_unstable, ... }@systemArg:
 lib.backport.backportingOverlay pkgs_unstable {
-  taskwarrior3 = "25.05"; # large speed up
   # should always be compatible & improve experience
   retroarch-joypad-autoconfig = "99.99";
 }
