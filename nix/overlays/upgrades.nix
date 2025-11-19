d6624823a382119a2fdfb2f867f0cdcf009e0d14
Felix Stupp <felix.stupp@banananet.work>
2025-11-19 19:43:06 +0000

    fix updating ansible-compat out of band

--- nix/overlays/upgrades.nix
+++ nix/overlays/upgrades.nix
@@ -4,15 +4,17 @@ final: prev: {
   # to fix ansible-lint, see:
   # - https://github.com/NixOS/nixpkgs/issues/460422
   # - https://github.com/NixOS/nixpkgs/pull/460755
-  ansible-compat = prev.ansible-compat.overrideAttrs (old: rec {
-    version = "25.8.1";
-    src = pkgs.fetchFromGitHub {
-      owner = "ansible";
-      repo = "ansible-compat";
-      tag = "v${version}";
-      hash = "sha256-hwfD7B0r8wRo/BUUA00TTQXCkrY8TAUM5BiP4Q4Atd0=";
-    };
-  });
+  python312Packages = prev.python312Packages // {
+    ansible-compat = prev.python312Packages.ansible-compat.overrideAttrs (old: rec {
+      version = "25.8.1";
+      src = pkgs.fetchFromGitHub {
+        owner = "ansible";
+        repo = "ansible-compat";
+        tag = "v${version}";
+        hash = "sha256-hwfD7B0r8wRo/BUUA00TTQXCkrY8TAUM5BiP4Q4Atd0=";
+      };
+    });
+  };
   ptouch-print = prev.ptouch-print.overrideAttrs (old: {
     version = "1.6";
     src = pkgs.fetchgit {
