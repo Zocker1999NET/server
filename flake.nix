3bf1c403ab87308063d8bb2d364b8dc986b11c95
Felix Stupp <felix.stupp@banananet.work>
2025-12-08 09:20:42 +0000

    update flake.mix & flake.lock to 25.11

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
 
     # packages repositories
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs_unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # required submodules
@@ -14,7 +14,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     impermanence.url = "github:nix-community/impermanence";
