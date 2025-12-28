0c52a0d05366189b8cf1ff195b3526cec1b1a0bf
Felix Stupp <felix.stupp@banananet.work>
2025-12-28 18:37:27 +0000

    extract mgmt-iso config

---
+++ nix/nixos/de.6nw/temp/mgmt-iso/default.nix
@@ -0,0 +1,52 @@
+{
+  flake,
+  lib,
+  outputs,
+  ...
+}@flakeArg:
+{
+  # TODO integrate into auto-iso, see taskwarrior:///321e5090-fe7d-4fc8-aeee-0117a344f33a
+  modules = [
+
+    # host config
+    (
+      { config, pkgs, ... }:
+      {
+        config = {
+          documentation.info.enable = lib.mkForce false;
+          environment.systemPackages = with pkgs; [
+            nwipe # make wiping disks easier
+          ];
+          isoImage.edition = "de.6nw-mgmt";
+          networking.hostName = "mgmt-iso";
+          programs.disko-install-menu = {
+            enable = true;
+            listedFlakes.defaultFlake = {
+              offlineHosts = {
+                "empty" = true;
+              };
+              offlineReference = flake;
+            };
+            offlineCapable = true;
+            options = {
+              # TODO autostart
+              defaultFlake = "github:Zocker1999NET/server"; # ===SYNC:general/meta/repo/url
+              defaultHost = "empty";
+            };
+          };
+          users.users.root.openssh.authorizedKeys.keys = config.x-banananetwork.sshPublicKeys;
+          x-banananetwork = {
+            allCommon.enable = true;
+            useable.enable = true;
+          };
+        };
+      }
+    )
+    ./../../../../offlineInstallDeps.nix
+
+    # hardware & "state"
+    outputs.nixosProfiles.installer
+
+  ];
+  system = "x86_64-linux";
+}
--- nix/nixos/default.nix
+++ nix/nixos/default.nix
@@ -61,48 +61,8 @@ in
 
   "empty" = importSystem ./de.6nw/temp/empty;
 
-  # TODO integrate into auto-iso, see taskwarrior:///321e5090-fe7d-4fc8-aeee-0117a344f33a
   # (note) build: .#nixosConfigurations.mgmt-iso.config.system.build.isoImage
-  "mgmt-iso" = nixosSystem {
-    modules = [
-      outputs.nixosProfiles.installer
-      (
-        { config, pkgs, ... }:
-        {
-          config = {
-            documentation.info.enable = lib.mkForce false;
-            environment.systemPackages = with pkgs; [
-              nwipe # make wiping disks easier
-            ];
-            isoImage.edition = "de.6nw-mgmt";
-            networking.hostName = "mgmt-iso";
-            programs.disko-install-menu = {
-              enable = true;
-              listedFlakes.defaultFlake = {
-                offlineHosts = {
-                  "empty" = true;
-                };
-                offlineReference = flake;
-              };
-              offlineCapable = true;
-              options = {
-                # TODO autostart
-                defaultFlake = "github:Zocker1999NET/server"; # ===SYNC:general/meta/repo/url
-                defaultHost = "empty";
-              };
-            };
-            users.users.root.openssh.authorizedKeys.keys = config.x-banananetwork.sshPublicKeys;
-            x-banananetwork = {
-              allCommon.enable = true;
-              useable.enable = true;
-            };
-          };
-        }
-      )
-      ./../offlineInstallDeps.nix
-    ];
-    system = "x86_64-linux";
-  };
+  "mgmt-iso" = importSystem ./de.6nw/temp/mgmt-iso;
 
   "iehsrv995" = nixosSystem {
     # TODO copy config from host
