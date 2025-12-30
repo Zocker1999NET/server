d7cf60945eb573f1817e988a289ca782166a827c
Felix Stupp <felix.stupp@banananet.work>
2025-12-30 12:43:19 +0000

    nixos-mod/vmDisko/luks-lvm-1: enable tpm2-measure-pcr

--- nix/nixos-modules/vmDisko/luks-lvm-1.nix
+++ nix/nixos-modules/vmDisko/luks-lvm-1.nix
@@ -25,6 +25,18 @@
                 type = "lvm_pv";
                 vg = "pool";
               };
+              settings = {
+                crypttabExtraOpts = [
+                  # required for TPM unlock to continue to work with another tpm2 option set
+                  # (does not require a tpm2 token/slot to exist)
+                  "tpm2-device=auto"
+                  # allows for PCR 15 validation (not fully implemented by that!)
+                  # (and has no disadvantages on systems without)
+                  # without PCR 15 validation, TPM & signed initrd can be misused by replacing LUKS partition
+                  # see: https://oddlama.org/blog/bypassing-disk-encryption-with-tpm2-unlock/
+                  "tpm2-measure-pcr=yes"
+                ];
+              };
             };
           };
         };
