459752a4d2091ccb6220a11dabbd2022635a6e7b
Felix Stupp <felix.stupp@banananet.work>
2026-01-24 13:42:00 +0000

    nix-builder/srv-autoUpdate: fix startAt time spec

--- nix/nixos/de.6nw/pve.boreth/nix-builder/srv-autoUpdate/default.nix
+++ nix/nixos/de.6nw/pve.boreth/nix-builder/srv-autoUpdate/default.nix
@@ -42,7 +42,7 @@ in
       Group = userName;
       User = userName;
     };
-    startAt = "Sam *-*-* 08:00"; # documented in nixos-modules/serverCommon.nix
+    startAt = "Sat *-*-* 08:00"; # documented in nixos-modules/serverCommon.nix
   };
 
   users = {
