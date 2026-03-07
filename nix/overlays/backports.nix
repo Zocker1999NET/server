c1a40ead6fcd26e19dbb2bc337319dfdbb80f234
Felix Stupp <felix.stupp@banananet.work>
2026-03-07 20:18:01 +0000

    backport yt-dlp

--- nix/overlays/backports.nix
+++ nix/overlays/backports.nix
@@ -2,6 +2,8 @@
 { pkgs_unstable, ... }@systemArg:
 lib.backport.backportingOverlay pkgs_unstable {
   ncspot = "25.11"; # or https://github.com/NixOS/nixpkgs/pull/438048
+  # always required to be backported, hopefully always compatible
+  yt-dlp = "99.99";
   # should always be compatible & improve experience
   retroarch-joypad-autoconfig = "99.99";
 }
