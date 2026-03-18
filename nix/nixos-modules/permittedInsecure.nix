16a3807b0558fa67153c23062572f6e2f1e63a46
Felix Stupp <felix.stupp@banananet.work>
2026-03-18 21:55:35 +0000

    nixos-mod/permittedInsecure: refactor & add docu for manual

--- nix/nixos-modules/permittedInsecure.nix
+++ nix/nixos-modules/permittedInsecure.nix
@@ -12,18 +12,36 @@ let
   inherit (lib.modules) mkIf;
   inherit (lib.options) mkOption;
   inherit (lib.strings) versionAtLeast;
+  inherit (lib.trivial) flip pipe;
   convertEntry = name: version: optional (versionAtLeast version pkgs.lib.version) name;
-  convertAttrs = attrs: concatLists (mapAttrsToList convertEntry attrs);
+  convertAttrs = flip pipe [
+    (mapAttrsToList convertEntry)
+    concatLists
+  ];
   cfg = config.nixpkgs.permitInsecurePackagesUntil;
 in
 {
   options.nixpkgs.permitInsecurePackagesUntil = mkOption {
+    description = ''
+      Permit insecure packages until a given NixOS major version.
+
+      This may be useful to limit the impact of permitted insecure packages
+      only until the next NixOS major upgrade
+      without relying on deleting such entry manually.
+
+      To limit a permit only for the current major release
+      (so the permit stops working after the current version),
+      use the version number of the current major release.
+    '';
     type = with types; attrsOf str;
     default = { };
+    example = {
+      "temporary-insecure-package" = "25.11";
+    };
   };
 
   config.nixpkgs.config = mkIf (cfg != { }) {
-    permittedInsecurePackages = (convertAttrs cfg);
+    permittedInsecurePackages = convertAttrs cfg;
   };
 
 }
