a25cc2e249242cf7f407bd30c7d8668a1807dfe7
Felix Stupp <felix.stupp@banananet.work>
2026-04-18 21:20:54 +0000

    overlays/backports: use constant for infinite backporting

--- nix/overlays/backports.nix
+++ nix/overlays/backports.nix
@@ -1,9 +1,12 @@
 { lib, ... }@flakeArg:
 { pkgs_unstable, ... }@systemArg:
+let
+  infinite = "999.99";
+in
 lib.backport.backportingOverlay pkgs_unstable {
   ncspot = "25.11"; # or https://github.com/NixOS/nixpkgs/pull/438048
   # always required to be backported, hopefully always compatible
-  yt-dlp = "99.99";
+  yt-dlp = infinite;
   # should always be compatible & improve experience
-  retroarch-joypad-autoconfig = "99.99";
+  retroarch-joypad-autoconfig = infinite;
 }
