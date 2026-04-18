080a704962bf837c135c11dd4d5f15f699aa862b
Felix Stupp <felix.stupp@banananet.work>
2026-04-18 22:28:19 +0000

    nixos/frontend/home-develop: add chat.tools.urls.autoApprove for trusted AI URLs

--- nix/nixos-modules/frontend/home-develop.nix
+++ nix/nixos-modules/frontend/home-develop.nix
@@ -111,6 +111,14 @@
           "black-formatter.path" = "${pkgs.black}/bin/black";
 
           "chat.disableAIFeatures" = false;
+          "chat.tools.urls.autoApprove" = {
+            # sorted alphabetically by reversed domain
+            "https://github.com/microsoft/vscode/wiki/*" = true;
+            "https://docs.github.com" = true;
+            "https://code.visualstudio.com" = true;
+            "https://nixos.org/manual" = true;
+            "https://wiki.nixos.org" = true;
+          };
 
           "dev.containers.dockerComposePath" = "${lib.getExe pkgs.podman-compose}";
           "dev.containers.dockerPath" = "${lib.getExe pkgs.podman}";
