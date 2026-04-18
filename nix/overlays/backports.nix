536fe345942e6bd6650f3be223729980b5f0b417
Felix Stupp <felix.stupp@banananet.work>
2026-04-18 21:21:29 +0000

    backport full vscode stack

--- nix/overlays/backports.nix
+++ nix/overlays/backports.nix
@@ -5,6 +5,12 @@ let
 in
 lib.backport.backportingOverlay pkgs_unstable {
   ncspot = "25.11"; # or https://github.com/NixOS/nixpkgs/pull/438048
+  # backport all VSCode stuff as dev tools to prevent issues with online services
+  vscode = infinite;
+  vscode-fhs = infinite;
+  vscodium = infinite;
+  vscodium-fhs = infinite;
+  vscode-extensions = infinite;
   # always required to be backported, hopefully always compatible
   yt-dlp = infinite;
   # should always be compatible & improve experience
