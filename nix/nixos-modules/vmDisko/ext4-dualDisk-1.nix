74f7055b445caf2e626d481b488638d37960bc0a
Felix Stupp <felix.stupp@banananet.work>
2026-04-20 23:35:27 +0000

    vmDisko: add ext4-dualDisk-1 cfg

---
+++ nix/nixos-modules/vmDisko/ext4-dualDisk-1.nix
@@ -0,0 +1,43 @@
+{
+  disk = {
+    main = {
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            type = "EF00";
+            size = "500M";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
+          };
+        };
+      };
+    };
+    data = {
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions.data = {
+          size = "100%";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/data";
+          };
+        };
+      };
+    };
+  };
+}
