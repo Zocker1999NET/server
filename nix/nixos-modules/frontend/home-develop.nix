982f6e2019576eda103d2cc05a652d5da40dfb91
Felix Stupp <felix.stupp@banananet.work>
2026-04-21 09:17:42 +0000

    frontend/vscode: configure githubIssues to not trigger in nix files

--- nix/nixos-modules/frontend/home-develop.nix
+++ nix/nixos-modules/frontend/home-develop.nix
@@ -189,6 +189,30 @@
           "github.gitAuthentication" = false; # prefer native SSH for that
           "github.gitProtocol" = "ssh";
 
+          # define langs for which GitHub Issues should not trigger
+          # (i.e. languages where `#` is used frequently)
+          "githubIssues.ignoreCompletionTrigger" = [
+            # default
+            "coffeescript"
+            "crystal"
+            "diff"
+            "dockerfile"
+            "dockercompose"
+            "ignore"
+            "ini"
+            "julia"
+            "makefile"
+            "perl"
+            "powershell"
+            "python"
+            "r"
+            "ruby"
+            "shellscript"
+            "yaml"
+            # non-default
+            "nix"
+          ];
+
           "html.format.enable" = false;
 
           "json.format.enable" = true;
