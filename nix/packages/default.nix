fbc19cd10f29e62e527608caf5d14314f112c8db
Felix Stupp <felix.stupp@banananet.work>
2026-05-09 09:37:27 +0000

    packages: extract secrix-wrapper into its own file

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -17,53 +17,9 @@ in
 
   pdfpagecount = callPackage ./pdfpagecount { };
 
-  secrix-wrapper =
-    let
-      secrixExe = outputs.apps.${system}.secrix.program;
-    in
-    pkgs.writeShellApplication {
-      name = "secr";
-      text = ''
-        secrix() {
-          set -x
-          exec ${secrixExe} "$@"
-        }
-
-        help() {
-          echo "Usages:"
-          echo "  $0 [create|rekey|edit|encrypt] <system> [<args> …] <file>"
-          echo "  $0 decrypt [<args> …] <file>"
-        }
-
-        main() {
-          if [[ $# -lt 1 ]]; then
-            help
-            exit 0
-          fi
-          cmd="$1"
-          shift 1
-          case "$cmd" in
-            help|-h|--help)
-              help
-              ;;
-            create)
-              secrix "$cmd" --all-users --system "$@"
-              ;;
-            rekey|edit)
-              secrix "$cmd" --identity "$SECRIX_ID" --all-users --system "$@"
-              ;;
-            encrypt)
-              secrix "$cmd" --all-users --system "$@"
-              ;;
-            decrypt)
-              secrix "$cmd" --identity "$SECRIX_ID" "$@"
-              ;;
-          esac
-        }
-
-        main "$@"
-      '';
-    };
+  secrix-wrapper = callPackage ./secrix-wrapper {
+    secrixExe = outputs.apps.${system}.secrix.program;
+  };
 
   taskcheck = callPackage ./taskcheck { };
 
---
+++ nix/packages/secrix-wrapper/default.nix
@@ -0,0 +1,49 @@
+{
+  writeShellApplication,
+  # from flake
+  secrixExe,
+  ...
+}:
+writeShellApplication {
+  name = "secr";
+  text = ''
+    secrix() {
+      set -x
+      exec ${secrixExe} "$@"
+    }
+
+    help() {
+      echo "Usages:"
+      echo "  $0 [create|rekey|edit|encrypt] <system> [<args> …] <file>"
+      echo "  $0 decrypt [<args> …] <file>"
+    }
+
+    main() {
+      if [[ $# -lt 1 ]]; then
+        help
+        exit 0
+      fi
+      cmd="$1"
+      shift 1
+      case "$cmd" in
+        help|-h|--help)
+          help
+          ;;
+        create)
+          secrix "$cmd" --all-users --system "$@"
+          ;;
+        rekey|edit)
+          secrix "$cmd" --identity "$SECRIX_ID" --all-users --system "$@"
+          ;;
+        encrypt)
+          secrix "$cmd" --all-users --system "$@"
+          ;;
+        decrypt)
+          secrix "$cmd" --identity "$SECRIX_ID" "$@"
+          ;;
+      esac
+    }
+
+    main "$@"
+  '';
+}
