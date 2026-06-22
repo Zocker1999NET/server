07bc3879937756a8061c5c07bcfa793871be2b09
Felix Stupp <felix.stupp@banananet.work>
2026-06-22 21:39:34 +0000

    frontend/vscode: configure ansible lightspeed & mcp

--- nix/nixosModules/frontend/home-develop.nix
+++ nix/nixosModules/frontend/home-develop.nix
@@ -128,6 +128,12 @@ in
           };
 
           "ansible.ansibleLint.path" = "${lib.getExe pkgs.ansible-lint}";
+          # fuck you RedHat for prompting sign in to an optional feature
+          # until you find a non-obviously setting to disable it!
+          # pls fix https://github.com/ansible/vscode-ansible/issues/879 thx
+          "ansible.lightspeed.enabled" = false;
+          "ansible.lightspeed.suggestions.enabled" = false;
+          "ansible.mcpServer.enabled" = true;
 
           "black-formatter.path" = "${pkgs.black}/bin/black";
 
