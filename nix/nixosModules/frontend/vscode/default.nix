e7121be2c6c4f7220436c7018d0c825a2280e3d5
Felix Stupp <felix.stupp@banananet.work>
2026-06-27 20:37:12 +0000

    frontend/vscode: configure more profiles

--- nix/nixosModules/frontend/vscode/default.nix
+++ nix/nixosModules/frontend/vscode/default.nix
@@ -18,6 +18,8 @@ in
     mutableExtensionsDir = false;
 
     profiles = {
+
+      # TODO remove IDE specific modules (kept to avoid breaking change)
       default = imports [
         ./defaults
         # IDEs
@@ -33,6 +35,45 @@ in
           enableUpdateCheck = false;
         }
       ];
+
+      ansible = imports [
+        ./defaults
+        ./ide/ansible.nix
+      ];
+
+      kotlin = imports [
+        ./defaults
+        ./ide/kotlin.nix
+      ];
+
+      latex = imports [
+        ./defaults
+        ./ide/latex.nix
+      ];
+
+      nix = imports [
+        ./defaults
+        ./ide/nix.nix
+      ];
+
+      python = imports [
+        ./defaults
+        ./ide/python.nix
+      ];
+
+      svelte = imports [
+        ./defaults
+        ./ide/svelte.nix
+      ];
+
+      # esp. for my repos
+      sysadmin = imports [
+        ./defaults
+        ./ide/ansible.nix
+        ./ide/python.nix
+        ./ide/nix.nix
+      ];
+
     };
 
   };
