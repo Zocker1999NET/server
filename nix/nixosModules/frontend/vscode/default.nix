ab9dd86a15e3a63d287f7c6e8633e42a6f28be49
Felix Stupp <felix.stupp@banananet.work>
2026-06-27 14:06:01 +0000

    frontend: move vscode configs into their own sub-tree of files

---
+++ nix/nixosModules/frontend/vscode/default.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  ...
+}:
+let
+  inherit (lib.lists) toList;
+  imports = i: _: { imports = toList i; };
+in
+{
+  _class = "homeManager";
+  # VSCode cause faster updates compared to VSCodium
+  programs.vscode = {
+
+    enable = true;
+    mutableExtensionsDir = false;
+
+    profiles = {
+      default = imports [
+        ./p_default.nix
+      ];
+    };
+
+  };
+}
