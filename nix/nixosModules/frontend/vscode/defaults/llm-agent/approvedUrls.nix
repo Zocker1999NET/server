096196f84fd4838b744f855a22d657dde352ebda
Felix Stupp <felix.stupp@banananet.work>
2026-07-04 22:32:36 +0000

    frontend/vscode: replace generic VS Code URL with docs URL in auto approved llm urls

--- nix/nixosModules/frontend/vscode/defaults/llm-agent/approvedUrls.nix
+++ nix/nixosModules/frontend/vscode/defaults/llm-agent/approvedUrls.nix
@@ -7,7 +7,7 @@
     "https://github.com/microsoft/vscode/wiki/*" = true;
     "https://docs.github.com" = true;
 
-    "https://code.visualstudio.com" = true;
+    "https://code.visualstudio.com/docs" = true;
 
     "https://nixos.org/manual" = true;
     "https://wiki.nixos.org" = true;
