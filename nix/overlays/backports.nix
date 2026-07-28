a01a3f082a093c7f312775bfaec70750f1244446
Felix Stupp <felix.stupp@banananet.work>
2026-07-28 00:31:04 +0000

    backport trilium-desktop to secure version until 26.11
    solving https://github.com/NixOS/nixpkgs/issues/546491 for my case

--- nix/overlays/backports.nix
+++ nix/overlays/backports.nix
@@ -4,6 +4,8 @@ let
   infinite = "999.99";
 in
 libBNet.backport.backportingConfigOverlay inputs.nixpkgs_unstable {
+  # because of https://github.com/NixOS/nixpkgs/issues/546491, increases DB version, so cannot remove backport until 26.11 is released
+  trilium-desktop = "26.11";
   # backport all VSCode stuff as dev tools to prevent issues with online services
   vscode = infinite;
   vscode-fhs = infinite;
