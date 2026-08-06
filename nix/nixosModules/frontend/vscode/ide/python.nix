3f1932d10490d786f78cf422a77b7a905f465032
Felix Stupp <felix.stupp@banananet.work>
2026-08-06 10:41:51 +0000

    chore(vscode/python): reformat, pack into explicit config

--- nix/nixosModules/frontend/vscode/ide/python.nix
+++ nix/nixosModules/frontend/vscode/ide/python.nix
@@ -10,33 +10,37 @@ in
 
   # _class = "homeManager.vscodeProfile";
 
-  extensions = with pkgs.vscode-extensions; [
-    charliermarsh.ruff
-    ms-python.debugpy
-    ms-python.vscode-python-envs # only wanted for redhat.ansible
-    matangover.mypy
-    # TODO (feature) maybe add isort, https://github.com/microsoft/vscode-isort, in nixpkgs
-    # pylance does not work with VSCodium due to MSFT
-    ms-pyright.pyright
-    ms-python.python
-  ];
+  config = {
 
-  userSettings = {
-    "[python]" = {
-      "editor.defaultFormatter" = "charliermarsh.ruff";
+    extensions = with pkgs.vscode-extensions; [
+      charliermarsh.ruff
+      ms-python.debugpy
+      ms-python.vscode-python-envs # only wanted for redhat.ansible
+      matangover.mypy
+      # TODO (feature) maybe add isort, https://github.com/microsoft/vscode-isort, in nixpkgs
+      # pylance does not work with VSCodium due to MSFT
+      ms-pyright.pyright
+      ms-python.python
+    ];
+
+    userSettings = {
+      "[python]" = {
+        "editor.defaultFormatter" = "charliermarsh.ruff";
+      };
+
+      "mypy.dmypyExecutable" = "${pkgs.mypy}/bin/dmypy";
+      "mypy.runUsingActiveInterpreter" = false; # use mypy/dmypy from nixpkgs (otherwise mypy is needed to be installed in the used venv)
+      "mypy.mypyExecutable" = "${pkgs.mypy}/bin/mypy";
+
+      "python.analysis.autoImportCompletions" = true;
+      "python.analysis.stubPath" = "./typings/";
+      "python.defaultInterpreterPath" = lib.getExe pkgs.python3;
+
+      "workbench.editorAssociations" = {
+        "*.ipynb" = "jupyter-notebook";
+      };
     };
 
-    "mypy.dmypyExecutable" = "${pkgs.mypy}/bin/dmypy";
-    "mypy.runUsingActiveInterpreter" = false; # use mypy/dmypy from nixpkgs (otherwise mypy is needed to be installed in the used venv)
-    "mypy.mypyExecutable" = "${pkgs.mypy}/bin/mypy";
-
-    "python.analysis.autoImportCompletions" = true;
-    "python.analysis.stubPath" = "./typings/";
-    "python.defaultInterpreterPath" = lib.getExe pkgs.python3;
-
-    "workbench.editorAssociations" = {
-      "*.ipynb" = "jupyter-notebook";
-    };
   };
 
 }
